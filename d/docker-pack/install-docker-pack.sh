b6f8c654275331d55c7b4c5a94014dd2a7bf8b31
Evgeny Ratnikov <ratnikov.ev@gmail.com>
2020-07-25 11:19:09 +0300

    docker-pack: go-1.14.6, docker-19.03.8

--- d/docker-pack/install-docker-pack.sh
+++ d/docker-pack/install-docker-pack.sh
@@ -1,7 +1,16 @@
 #!/bin/sh -e
 
-GOROOT="/usr/lib64/go1.11.4/go"
-PATH="$GOROOT/bin:$PATH"
+function die
+{
+    echo $1
+    echo 1
+}
+
+ls -l /var/log/packages/ | grep -q gcc-go && die "gcc-go should be uninstalled"
+
+if [ -z "$GOROOT" ]; then
+    . /etc/profile.d/go.sh
+fi
 OUTPUT=/tmp
 
 function build__
@@ -19,41 +28,3 @@ build__ runc
 build__ containerd
 build__ docker-proxy
 build__ docker
-
-exit 0
-
-removepkg libseccomp || true
-cd libseccomp
-OUTPUT=`pwd` buildpkg libseccomp.SlackBuild
-installpkg ./libseccomp-*t?z
-cd ..
-
-removepkg runc || true<F2>
-cd runc
-OUTPUT=`pwd` buildpkg runc.SlackBuild
-installpkg ./runc-*t?z
-cd ..
-
-removepkg containerd || true
-cd containerd
-OUTPUT=`pwd` buildpkg containerd.SlackBuild
-installpkg ./containerd-*t?z
-cd ..
-
-removepkg tini || true
-cd tini
-OUTPUT=`pwd` buildpkg tini.SlackBuild
-installpkg ./tini-*t?z
-cd ..
-
-removepkg docker-proxy || true
-cd docker-proxy
-OUTPUT=`pwd` buildpkg docker-proxy.SlackBuild
-installpkg ./docker-proxy-*t?z
-cd ..
-
-removepkg docker || true
-cd docker
-OUTPUT=`pwd` buildpkg docker.SlackBuild
-installpkg ./docker-*t?z
-cd ..
